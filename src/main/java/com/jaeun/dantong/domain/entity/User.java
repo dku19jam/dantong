38c60c9bce62f7f0ade57e39bc10e1542d5acfde
최재민 <panzzang518@naver.com>
2022-12-28 17:03:25 +0900

    Fix: DDL 에러를 발생시키는 user table의 이름을 users로 변경

--- src/main/java/com/jaeun/dantong/domain/entity/User.java
+++ src/main/java/com/jaeun/dantong/domain/entity/User.java
@@ -4,18 +4,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Getter
+@Table(name = "users")
 public class User {
 
     @Id
